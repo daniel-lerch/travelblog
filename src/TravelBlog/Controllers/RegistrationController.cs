f36c8cc69212fdc25385f0918bda05a004a8f0b2
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-09-12 23:55:01 +0200

    Add RegistrationController and associated views

---
+++ src/TravelBlog/Controllers/RegistrationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelBlog.Database;
+using TravelBlog.Database.Entities;
+using TravelBlog.Models;
+
+namespace TravelBlog.Controllers
+{
+    [Route("~/register")]
+    public class RegistrationController : Controller
+    {
+        private DatabaseContext context;
+
+        public RegistrationController(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index", new RegistrationViewModel("", "", ""));
+        }
+
+        [HttpPost]
+        public IActionResult Post(string mailAddress, string givenName, string familyName)
+        {
+            //context.Users.Add(new User { MailAddress = mailAddress, GivenName = givenName, FamilyName = familyName });
+            return View("Index", new RegistrationViewModel(mailAddress, givenName, familyName, "Fehler: Keine Datenbankverbindung"));
+        }
+    }
+}
---
+++ src/TravelBlog/Models/RegistrationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBlog.Models
+{
+    public class RegistrationViewModel
+    {
+        public RegistrationViewModel(string mailAdress, string givenName, string familyName)
+            : this(mailAdress, givenName, familyName, null) { }
+
+        public RegistrationViewModel(string mailAddress, string givenName, string familyName, string displayMessage)
+        {
+            MailAddress = mailAddress;
+            GivenName = givenName;
+            FamilyName = familyName;
+            DisplayMessage = displayMessage;
+        }
+
+        public string MailAddress { get; }
+        public string GivenName { get; }
+        public string FamilyName { get; }
+        public string DisplayMessage { get; }
+    }
+}
