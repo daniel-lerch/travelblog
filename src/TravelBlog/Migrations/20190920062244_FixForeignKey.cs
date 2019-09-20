da1ebd773d12033e712a1af5ec8606dd8afd2947
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-09-20 08:46:02 +0200

    Change last migration to work with SQLite at the price of dataloss

--- src/TravelBlog/Migrations/20190920062244_FixForeignKey.cs
+++ src/TravelBlog/Migrations/20190920062244_FixForeignKey.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 namespace TravelBlog.Migrations
 {
@@ -6,41 +7,83 @@ namespace TravelBlog.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_PostReads_BlogPosts_SubscriberId",
-                table: "PostReads");
+            migrationBuilder.DropTable(
+                name: "PostReads");
+
+            migrationBuilder.CreateTable(
+                name: "PostReads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PostId = table.Column<int>(nullable: false),
+                    SubscriberId = table.Column<int>(nullable: false),
+                    AccessTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostReads", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostReads_BlogPosts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "BlogPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PostReads_Subscribers_SubscriberId",
+                        column: x => x.SubscriberId,
+                        principalTable: "Subscribers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
 
             migrationBuilder.CreateIndex(
                 name: "IX_PostReads_PostId",
                 table: "PostReads",
                 column: "PostId");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_PostReads_BlogPosts_PostId",
+            migrationBuilder.CreateIndex(
+                name: "IX_PostReads_SubscriberId",
                 table: "PostReads",
-                column: "PostId",
-                principalTable: "BlogPosts",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
+                column: "SubscriberId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_PostReads_BlogPosts_PostId",
-                table: "PostReads");
+            migrationBuilder.DropTable(
+                name: "PostReads");
 
-            migrationBuilder.DropIndex(
-                name: "IX_PostReads_PostId",
-                table: "PostReads");
+            migrationBuilder.CreateTable(
+                name: "PostReads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PostId = table.Column<int>(nullable: false),
+                    SubscriberId = table.Column<int>(nullable: false),
+                    AccessTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostReads", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostReads_BlogPosts_SubscriberId",
+                        column: x => x.SubscriberId,
+                        principalTable: "BlogPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PostReads_Subscribers_SubscriberId",
+                        column: x => x.SubscriberId,
+                        principalTable: "Subscribers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_PostReads_BlogPosts_SubscriberId",
+            migrationBuilder.CreateIndex(
+                name: "IX_PostReads_SubscriberId",
                 table: "PostReads",
-                column: "SubscriberId",
-                principalTable: "BlogPosts",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
+                column: "SubscriberId");
         }
     }
 }
