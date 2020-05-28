e42c351e90b95bd5a8f145f213d55987316a6126
Daniel Lerch <daniel.p.lerch@gmail.com>
2020-05-29 00:22:59 +0200

    Convert dates with JavaScript and update NuGet packages

---
+++ src/TravelBlog/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TravelBlog.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToUnixTimeMilliseconds(this DateTime dateTime)
+        {
+            return new DateTimeOffset(DateTime.SpecifyKind(dateTime, DateTimeKind.Utc)).ToUnixTimeMilliseconds();
+        }
+    }
+}
---
+++ src/TravelBlog/wwwroot/js/dateconverter.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+(function () {
+    const dateElements = document.getElementsByClassName("js-convert-date");
+    for (let element of dateElements) {
+        const date = new Date(parseInt(element.dataset.timestamp));
+        element.innerText = date.toLocaleDateString();
+        element.classList.remove("js-convert-date");
+    }
+
+    const datetimeElements = document.getElementsByClassName("js-convert-datetime");
+    for (let element of datetimeElements) {
+        const date = new Date(parseInt(element.dataset.timestamp));
+        element.innerText = date.toLocaleString();
+        element.classList.remove("js-convert-datetime");
+    }
+})();
