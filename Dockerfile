df19f66c8a01aea0eb8ba12a2d8167bc02ba5ce6
Daniel Lerch <daniel.p.lerch@gmail.com>
2021-10-21 11:54:00 +0200

    Use libvips instead of skia to generate thumbnails

--- Dockerfile
+++ Dockerfile
@@ -4,20 +4,17 @@ WORKDIR /app
 # Copy csproj and restore as distinct layers
 COPY src/TravelBlog/TravelBlog.csproj src/TravelBlog/libman.json ./src/TravelBlog/
 RUN set -x \
-    && dotnet restore -r debian.10-x64 src/TravelBlog/TravelBlog.csproj \
+    && dotnet restore src/TravelBlog/TravelBlog.csproj \
     && dotnet tool install -g Microsoft.Web.LibraryManager.Cli \
     && export PATH="$PATH:/root/.dotnet/tools" \
     && libman restore --root src/TravelBlog
 
 # Copy everything else and build
 COPY . ./
-RUN dotnet publish -c Release -r debian.10-x64 -o /app/out src/TravelBlog/TravelBlog.csproj
+RUN dotnet publish --no-restore -c Release -o /app/out src/TravelBlog/TravelBlog.csproj
 
 # Build runtime image
-FROM mcr.microsoft.com/dotnet/core/runtime-deps:3.1-buster-slim
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
 WORKDIR /app
-RUN set -x \
-    && apt-get update \
-    && apt-get install -y libfontconfig1
 COPY --from=build-env /app/out .
-ENTRYPOINT ["/app/TravelBlog"]
+ENTRYPOINT ["dotnet", "TravelBlog.dll"]
--- src/TravelBlog/Services/ThumbnailService.cs
+++ src/TravelBlog/Services/ThumbnailService.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Options;
-using SkiaSharp;
+using NetVips;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -32,19 +31,15 @@ namespace TravelBlog.Services
             if (!thumbnail.Exists)
             {
                 var temp = new FileInfo(Path.Combine(tempDir.FullName, Path.GetRandomFileName()));
-                using (var originalStream = new FileStream(original.FullName, FileMode.Open, FileAccess.Read, FileShare.Read))
-                using (var tempStream = new FileStream(temp.FullName, FileMode.CreateNew, FileAccess.Write, FileShare.None))
-                {
-                    await semaphore.WaitAsync();
+                await semaphore.WaitAsync();
 
-                    try
-                    {
-                        CreateThumbnail(originalStream, tempStream, size);
-                    }
-                    finally
-                    {
-                        semaphore.Release();
-                    }
+                try
+                {
+                    CreateThumbnail(original.FullName, temp.FullName, size);
+                }
+                finally
+                {
+                    semaphore.Release();
                 }
 
                 temp.MoveTo(thumbnail.FullName, true);
@@ -52,109 +47,17 @@ namespace TravelBlog.Services
             return thumbnail.FullName;
         }
 
-        private void CreateThumbnail(FileStream original, FileStream temp, int size)
+        private void CreateThumbnail(string sourceFilePath, string destinationFilePath, int size)
         {
-            using SKCodec codec = SKCodec.Create(original);
-            using SKBitmap image = SKBitmap.Decode(codec);
-
-            int width = image.Width, height = image.Height;
-            double factor = (double)size / Math.Max(image.Width, image.Height);
+            using var source = Image.NewFromFile(sourceFilePath, access: Enums.Access.Sequential);
 
             // Omit upscaling of images but store the result for caching
-            if (factor < 1.0)
-            {
-                width = (int)Math.Round(image.Width * factor);
-                height = (int)Math.Round(image.Height * factor);
-            }
+            double factor = Math.Min(1.0, (double)size / Math.Max(source.Width, source.Height));
 
-            Action<SKCanvas> transform = Rotate(codec.EncodedOrigin, ref width, ref height);
+            using var resized = source.Resize(factor, Enums.Kernel.Lanczos3);
 
-            var info = new SKImageInfo(width, height);
-            using SKSurface surface = SKSurface.Create(info);
-
-            transform(surface.Canvas);
-
-            using (var paint = new SKPaint { IsAntialias = true, FilterQuality = SKFilterQuality.High })
-            {
-                surface.Canvas.DrawBitmap(image, info.Rect, paint);
-            }
-
-            surface.Canvas.Flush();
-
-            using SKImage save = surface.Snapshot();
-            save.Encode(SKEncodedImageFormat.Jpeg, options.Value.JpegQuality).SaveTo(temp);
-        }
-
-        // Unfortunately we cannot copy all exif meta data: https://github.com/mono/SkiaSharp/issues/836#issuecomment-584895517
-        private Action<SKCanvas> Rotate(SKEncodedOrigin origin, ref int width, ref int height)
-        {
-            Action<SKCanvas> transform;
-            int newWidth = width;
-            int newHeight = height;
-
-            switch (origin)
-            {
-                case SKEncodedOrigin.TopLeft:
-                    transform = canvas => { };
-                    break;
-                case SKEncodedOrigin.TopRight:
-                    // flip along the x-axis
-                    transform = canvas => canvas.Scale(-1, 1, newWidth / 2, newHeight / 2);
-                    break;
-                case SKEncodedOrigin.BottomRight:
-                    transform = canvas => canvas.RotateDegrees(180, newWidth / 2, newHeight / 2);
-                    break;
-                case SKEncodedOrigin.BottomLeft:
-                    // flip along the y-axis
-                    transform = canvas => canvas.Scale(1, -1, newWidth / 2, newHeight / 2);
-                    break;
-                case SKEncodedOrigin.LeftTop:
-                    newWidth = height;
-                    newHeight = width;
-                    transform = canvas =>
-                    {
-                        // Rotate 90
-                        canvas.RotateDegrees(90, newWidth / 2, newHeight / 2);
-                        canvas.Scale((float)newHeight / newWidth, (float)-newWidth / newHeight, newWidth / 2, newHeight / 2);
-                    };
-                    break;
-                case SKEncodedOrigin.RightTop:
-                    newWidth = height;
-                    newHeight = width;
-                    transform = canvas =>
-                    {
-                        // Rotate 90
-                        canvas.RotateDegrees(90, newWidth / 2, newHeight / 2);
-                        canvas.Scale((float)newHeight / newWidth, (float)newWidth / newHeight, newWidth / 2, newHeight / 2);
-                    };
-                    break;
-                case SKEncodedOrigin.RightBottom:
-                    newWidth = height;
-                    newHeight = width;
-                    transform = canvas =>
-                    {
-                        // Rotate 90
-                        canvas.RotateDegrees(90, newWidth / 2, newHeight / 2);
-                        canvas.Scale((float)-newHeight / newWidth, (float)newWidth / newHeight, newWidth / 2, newHeight / 2);
-                    };
-                    break;
-                case SKEncodedOrigin.LeftBottom:
-                    newWidth = height;
-                    newHeight = width;
-                    transform = canvas =>
-                    {
-                        // Rotate 90
-                        canvas.RotateDegrees(90, newWidth / 2, newHeight / 2);
-                        canvas.Scale((float)-newHeight / newWidth, (float)-newWidth / newHeight, newWidth / 2, newHeight / 2);
-                    };
-                    break;
-                default:
-                    throw new NotSupportedException();
-            }
-
-            width = newWidth;
-            height = newHeight;
-            return transform;
+            using var thumbnail = NetVips.Image.Thumbnail(sourceFilePath, size, size, size: Enums.Size.Down);
+            thumbnail.Jpegsave(destinationFilePath, options.Value.JpegQuality);
         }
     }
 }
