acb00df04cb1ba6fd86ddb41351b9c7162df8fab
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2022-06-30 22:22:37 +0200

    Prepare Vue for dynamic base paths

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from './views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,12 +13,12 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ './views/AboutView.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(window.resourceBasePath),
   routes
 })
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+const WebpackRequireFrom = require('webpack-require-from')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new WebpackRequireFrom({
+        variableName: 'resourceBasePath'
+      })
+    ]
+  }
 })
